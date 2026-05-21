22cca97b794dc95d8b4ff33f11ddb2f9544b6de7
daeliniko04-cmd <daeliniko04@gmail.com>
2026-05-22 01:42:04 +0700

    Delete db.php

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-function getDB(): PDO {
-    $dbPath = __DIR__ . '/jamuku.db';
-    $db = new PDO('sqlite:' . $dbPath);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    return $db;
-}
